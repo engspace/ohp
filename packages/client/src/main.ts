b844ce01f8ca1bdd02b7ef6c012864e10286232b
Remi Thebault <remi.thebault@gmail.com>
2020-07-21 00:20:32 +0200

    client registration

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -1,19 +1,25 @@
 import VueCompositionApi from '@vue/composition-api';
 import Vue from 'vue';
 import App from './App.vue';
+import { provideApollo } from './apollo';
 import router from './router';
+import { provideAuth } from './services/auth';
 import { provideGoogleSignIn } from './services/google';
 import { vuetify } from './vuetify';
 
-Vue.config.productionTip = false;
 Vue.use(VueCompositionApi);
+console.log('registered vue');
+
+Vue.config.productionTip = false;
 
 new Vue({
     router,
     vuetify,
     setup() {
+        provideApollo();
+        provideAuth();
         provideGoogleSignIn({
-            clientId: process.env.VUE_APP_GOOGLE_SIGNIN_CLIENT_ID,
+            clientId: process.env.VUE_APP_GOOGLE_SIGNIN_CLIENT_ID as string,
         });
         return {};
     },
--- packages/client/src/pages/RegisterPage.vue
+++ packages/client/src/pages/RegisterPage.vue
@@ -19,7 +19,7 @@
         </v-row>
         <v-row align="center" justify="center">
             <v-col cols="12" sm="8" md="4">
-                <v-card>
+                <v-card :loading="registerLoading">
                     <v-toolbar dark color="secondary" flat>
                         <v-toolbar-title>
                             Register with e-mail and password
@@ -27,6 +27,19 @@
                     </v-toolbar>
                     <v-card-text>
                         <v-form v-model="canRegister">
+                            <v-text-field
+                                v-model="name"
+                                prepend-icon="mdi-account"
+                                label="Nickname"
+                                :rules="[required]"
+                                dense
+                            ></v-text-field>
+                            <v-text-field
+                                v-model="fullName"
+                                prepend-icon="mdi-account"
+                                label="Full name"
+                                dense
+                            ></v-text-field>
                             <v-text-field
                                 v-model="email"
                                 prepend-icon="mdi-at"
@@ -53,6 +66,12 @@
                                 ></div>
                             </div>
                         </v-form>
+                        <v-alert :value="!!displayedError" type="error">{{
+                            displayedError
+                        }}</v-alert>
+                        <v-alert :value="!!confirmation" type="success">{{
+                            confirmation
+                        }}</v-alert>
                     </v-card-text>
                     <v-card-actions>
                         <v-btn
@@ -72,16 +91,31 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@vue/composition-api';
+import { useMutation } from '@vue/apollo-composable';
+import { computed, defineComponent, ref } from '@vue/composition-api';
+import gql from 'graphql-tag';
 import validator from 'validator';
+import { USER_FIELDS, useUserConflicts } from '@engspace/client-comps';
 import {
     GoogleUser,
     useGoogleSignIn,
     useGoogleRecaptchaV2,
 } from '@/services/google';
 
+const ACCOUNT_FIELDS = gql`
+    fragment AccountFields on Account {
+        id
+        type
+        user {
+            ...UserFields
+        }
+    }
+    ${USER_FIELDS}
+`;
+
 export default defineComponent({
     setup() {
+        console.log('in setup');
         const {
             signIn: googleSignIn,
             onSuccess: googleOnSuccess,
@@ -94,32 +128,118 @@ export default defineComponent({
             // TODO
         });
 
+        console.log('in setup 1');
+        const name = ref('');
+        const fullName = ref('');
         const email = ref('');
         const password = ref('');
         const showPswd = ref(false);
+        console.log('in setup 2');
 
         const canRegister = ref(false);
         const required = (val: string) => !!val || 'Required';
         const isEmail = (val: string) =>
             (!!val && validator.isEmail(val)) || 'Enter a valid e-mail address';
+        const user = computed(() => ({
+            email: email.value,
+            name: name.value,
+        }));
+        console.log('in setup 3');
+        const {
+            name: nameConflict,
+            email: emailConflict,
+            // we don't display anything if this request fails
+            // error: conflictQueryError,
+        } = useUserConflicts({
+            user,
+        });
+        console.log('in setup 4');
+        const nameConflictErrorMsg = computed(() => {
+            if (name.value && nameConflict.value) {
+                return `${name} already exists in the database`;
+            }
+            return '';
+        });
+
+        const emailConflictErrorMsg = computed(() => {
+            if (email.value && emailConflict.value) {
+                return `${email} already exists in the database`;
+            }
+            return '';
+        });
 
         const recaptchaSiteKey = process.env.VUE_APP_GOOGLE_RECAPTCHA_SITE_KEY;
-        const { getResponse } = useGoogleRecaptchaV2();
+        const { getResponse: getRecaptchaResponse } = useGoogleRecaptchaV2();
+
+        const {
+            mutate: registerMutate,
+            loading: registerLoading,
+            onDone: registerOnDone,
+            error: registerError,
+        } = useMutation(
+            gql`
+                mutation CreateLocalAccount($input: LocalAccountInput!) {
+                    accountCreateLocal(input: $input) {
+                        ...AccountFields
+                    }
+                }
+                ${ACCOUNT_FIELDS}
+            `
+        );
+
+        const recaptchaError = ref('');
+
+        const displayedError = computed(
+            () => /*registerError.value?.message || */ recaptchaError.value
+        );
+
+        const confirmation = ref('');
 
         function register() {
-            // TODO
+            confirmation.value = '';
+            recaptchaError.value = '';
+            const rr = getRecaptchaResponse();
+            if (!rr) {
+                recaptchaError.value = 'You must be identified as a human';
+                return;
+            }
+            registerMutate({
+                input: {
+                    name: name.value,
+                    email: email.value,
+                    fullName: fullName.value,
+                    password: password.value,
+                    recaptchaToken: rr,
+                },
+            });
         }
+
+        registerOnDone(() => {
+            recaptchaError.value = '';
+            confirmation.value =
+                'Your account has been registered but is still inactive\n' +
+                'An activation link was sent to ' +
+                email.value;
+        });
+        console.log('out setup');
 
         return {
             googleSignIn,
+            name,
+            fullName,
             email,
             password,
             showPswd,
             canRegister,
             required,
             isEmail,
+            nameConflictErrorMsg,
+            emailConflictErrorMsg,
             recaptchaSiteKey,
             register,
+            registerLoading,
+            confirmation,
+            displayedError,
         };
     },
 });

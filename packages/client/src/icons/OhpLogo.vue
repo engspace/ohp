de4b95febdcc51eec81a3483630a0e400a4570c2
Remi Thebault <remi.thebault@gmail.com>
2020-07-24 10:44:54 +0200

    ohp logo

---
+++ packages/client/src/icons/OhpLogo.vue
@@ -0,0 +1,39 @@
+<template>
+    <svg width="64" height="64" viewBox="0 0 64 64">
+        <circle
+            r="8"
+            cy="10"
+            cx="10"
+            stroke="black"
+            stroke-width="4"
+            fill="none"
+        />
+        <circle
+            cx="47"
+            cy="20"
+            r="14"
+            stroke="black"
+            stroke-width="6"
+            fill="none"
+        />
+        <circle
+            cx="24"
+            cy="50"
+            r="10"
+            stroke="black"
+            stroke-width="6"
+            fill="none"
+        />
+        <line x1="18" y1="12" x2="31" y2="16" stroke="black" stroke-width="5" />
+        <line x1="13" y1="19" x2="20" y2="40" stroke="black" stroke-width="5" />
+    </svg>
+</template>
+
+<style>
+circle {
+    stroke: currentColor;
+}
+line {
+    stroke: currentColor;
+}
+</style>
--- packages/client/src/vuetify.ts
+++ packages/client/src/vuetify.ts
@@ -1,14 +1,26 @@
 import '@mdi/font/css/materialdesignicons.css';
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import OhpLogo from '@/icons/OhpLogo.vue';
 
 Vue.use(Vuetify);
 
 export const vuetify = new Vuetify({
     icons: {
         iconfont: 'mdi',
+        values: {
+            ohplogo: {
+                component: OhpLogo,
+            },
+        },
     },
     theme: {
+        themes: {
+            light: {
+                primary: '#344955',
+                secondary: '#F9AA33',
+            },
+        },
         options: {
             customProperties: true,
         },

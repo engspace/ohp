8afd88141fd7be7c8b72642cea1280efa28b074f
Remi Thebault <remi.thebault@gmail.com>
2020-08-03 18:05:15 +0200

    client: router sets document.title

--- packages/client/src/router/index.ts
+++ packages/client/src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
+import NotFoundPage from '../pages/NotFoundPage.vue';
+import OrganizationPage from '../pages/OrganizationPage.vue';
 import ProjectCreatePage from '../pages/ProjectCreatePage.vue';
 import RegisterPage from '../pages/RegisterPage.vue';
 import SigninPage from '../pages/SigninPage.vue';
@@ -17,16 +19,25 @@ const routes: Array<RouteConfig> = [
         path: '/signin',
         name: 'Sign-in',
         component: SigninPage,
+        meta: {
+            title: 'Sign-in',
+        },
     },
     {
         path: '/register',
         name: 'Register',
         component: RegisterPage,
+        meta: {
+            title: 'Register',
+        },
     },
     {
         path: '/create-project',
         name: 'Create project',
         component: ProjectCreatePage,
+        meta: {
+            title: 'Create Project',
+        },
     },
 ];
 
@@ -34,6 +45,17 @@ const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    const ohp = 'Open-Hardware Platform';
+    const pageTitle = to.meta?.title;
+    if (pageTitle) {
+        document.title = `${ohp} - ${pageTitle}`;
+    } else {
+        document.title = ohp;
+    }
+    next();
 });
 
 export default router;
